90ec6cf553b0b0be831d6ac863e23b91296ecde4
Jesús Torres <jesus.torres.roman11@gmail.com>
2025-07-22 19:50:17 +0200

    api-index server

---
+++ backend/api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Mongo conectado'))
+  .catch((err) => console.error('Error Mongo:', err));
+
+// Rutas
+app.use('/api/auth', require('../routes/authRoutes'));
+app.use('/api/protected', require('../routes/protectedRoutes'));
+app.use('/api/rutinas', require('../routes/rutinaRoutes'));
+app.use('/api/user', require('../routes/userRoutes'));
+
+app.get('/api', (req, res) => {
+  res.send('API funcionando');
+});
+
+// Exportar como handler (esto es clave)
+module.exports = app;
